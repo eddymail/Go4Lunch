c091a934f2d46693844c1d9973864acbac2103e9
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-09-24 21:36:11 +0200

    activity_auth.xml

---
+++ app/src/main/java/com/lousssouarn/edouard/go4lunch/view/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.lousssouarn.edouard.go4lunch.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lousssouarn.edouard.go4lunch.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
