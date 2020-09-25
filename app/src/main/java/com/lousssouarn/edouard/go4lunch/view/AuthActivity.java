51563c8096717b935f002aa512fd905f6d2894cc
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-09-25 08:53:10 +0200

    AuthActivity.java: -google Sign in -facebook Sign in

--- app/src/main/java/com/lousssouarn/edouard/go4lunch/view/AuthActivity.java
+++ app/src/main/java/com/lousssouarn/edouard/go4lunch/view/AuthActivity.java
@@ -3,14 +3,62 @@ package com.lousssouarn.edouard.go4lunch.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
 import com.lousssouarn.edouard.go4lunch.R;
 
+import java.util.Collections;
+
 public class AuthActivity extends AppCompatActivity {
+
+    //For UI
+    Button googleButton;
+    Button facebookButton;
+
+    //For data
+    private final static int RC_SIGN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        googleButton = findViewById(R.id.auth_activity_google_button);
+        facebookButton = findViewById(R.id.auth_activity_facebook_button);
+
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignInWithGoogle();
+            }
+        });
+    }
+
+    /**
+     * Sign in With Google
+     */
+    private void startSignInWithGoogle() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(), RC_SIGN);
+    }
+
+    /**
+     * Sign in With Facebook
+     */
+    private void startSignInWithFacebook() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Collections.singletonList(
+                                new AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(), RC_SIGN);
     }
 }
