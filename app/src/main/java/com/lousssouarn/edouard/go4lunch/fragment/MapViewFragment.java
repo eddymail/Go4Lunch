36a922cdb1d5a43db9f89c8ac562cdf89c62f783
Edouard Loussouarn <ed.loussouarn@gmail.com>
2020-10-05 14:57:52 +0200

    MainActivity : add AutocompleteSupportFragment

--- app/src/main/java/com/lousssouarn/edouard/go4lunch/fragment/MapViewFragment.java
+++ app/src/main/java/com/lousssouarn/edouard/go4lunch/fragment/MapViewFragment.java
@@ -1,6 +1,7 @@
 package com.lousssouarn.edouard.go4lunch.fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -10,7 +11,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
@@ -21,12 +24,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
 import com.lousssouarn.edouard.go4lunch.R;
 
 /**
@@ -34,22 +31,73 @@ import com.lousssouarn.edouard.go4lunch.R;
  * Use the {@link MapViewFragment#} factory method to
  * create an instance of this fragment.
  */
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends Fragment implements LocationListener {
 
+    public static final int PERMS_CALL_ID = 1234;
     private GoogleMap map;
-    SupportMapFragment supportMapFragment;
+    private SupportMapFragment supportMapFragment;
     LocationManager locationManager;
-    LocationListener locationListener;
     LatLng userLatLng;
     FloatingActionButton locationButton;
+    View mapView;
 
 
     public MapViewFragment() {
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, PERMS_CALL_ID);
+            return;
+        }
+
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 10000, 0, this);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+        }
+
+        loadMap();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMS_CALL_ID) {
+            checkPermissions();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
 
     }
 
@@ -58,7 +106,7 @@ public class MapViewFragment extends Fragment {
         //Initialize view
         View v = inflater.inflate(R.layout.fragment_map_view, container, false);
 
-        locationButton = (FloatingActionButton) v.findViewById(R.id.fab_gps_location);
+        locationButton = v.findViewById(R.id.fab_gps_location);
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,86 +117,60 @@ public class MapViewFragment extends Fragment {
         //Initialize map fragment
         supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
-
-
-        //Async map
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(final GoogleMap googleMap) {
-                map = googleMap;
-
-                locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-                locationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        // store user latLng
-                        userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                        // clear old location marker in google map
-                        map.clear();
-                        map.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
-
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-
-                    }
-                };
-
-                //Asking for map permission with user / location permission
-
-                askLocationPermission();
-            }
-        });
+        mapView = supportMapFragment.getView();
 
         return v;
-}
-
-    private void askLocationPermission() {
-        Dexter.withActivity(getActivity()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse response) {
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                    return;
-                }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-                //getting user last location to set the default location marker in the map
-
-                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-                userLatLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-                // clear old location marker in google map
-                map.clear();
-                map.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
-                map.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
-                //Zoom on location
-                float zoomLevel = 16.0f;
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, zoomLevel));
-            }
-
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse response) {
-
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                token.cancelPermissionRequest();
-            }
-        }).check();
     }
 
+    @SuppressLint("MissingPermission")
+    private void loadMap() {
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                //getting user last location to set the default location marker in the map
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                userLatLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                //clear old location marker in google map
+                map.clear();
+                map.addMarker(new MarkerOptions().position(userLatLng).title("Your Location"));
+                // Location button
+                View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+                RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+                // position on right bottom
+                rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+                rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);rlp.setMargins(0,0,30,300);
+                map.setMyLocationEnabled(true);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 16));
+
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if ( map != null) {
+            //store user latitude and longitude
+            LatLng newUserLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            //clear old location marker in google map
+            map.clear();
+            map.addMarker(new MarkerOptions().position(newUserLatLng).title("Your Location"));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
--- app/src/main/java/com/lousssouarn/edouard/go4lunch/view/MainActivity.java
+++ app/src/main/java/com/lousssouarn/edouard/go4lunch/view/MainActivity.java
@@ -1,17 +1,35 @@
 package com.lousssouarn.edouard.go4lunch.view;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lousssouarn.edouard.go4lunch.R;
+import com.lousssouarn.edouard.go4lunch.fragment.MapViewFragment;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //FOR DATA
+    LatLng searchLocationLatLng;
+    MapFragment mapFragment;
 
     //FOR DESIGN
     BottomNavigationView bottomNavigationView;
@@ -25,6 +43,44 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         navController = Navigation.findNavController(this, R.id.fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        initializePlaces();
+        setupAutoCompleteSupportFragment();
+
+    }
+
+    private void initializePlaces(){
+        String apiKey = getString(R.string.api_key);
+
+        //Initialize Places. For simplicity, the API key is hard-coded.
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+
+        // Create a new Places client instance.
+        //PlacesClient placesClient = Places.createClient(this);
+
+    }
+
+    private void setupAutoCompleteSupportFragment() {
+        // Initialize the AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS, Place.Field.OPENING_HOURS, Place.Field.PHOTO_METADATAS, Place.Field.RATING));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                searchLocationLatLng = place.getLatLng();
+                
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.e("Error", status.getStatusMessage());
+            }
+        });
 
     }
 }
